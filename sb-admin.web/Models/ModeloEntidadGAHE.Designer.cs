f337ca88f1d66ffbb08d8a2d4712e4f5a9c735aa
joserodriguezcnt@gmail.com <joserodriguezcnt@gmail.com>
2019-11-04 15:30:43 -0500

    Cambio de JOSE

--- sb-admin.web/Models/ModeloEntidadGAHE.Designer.cs
+++ sb-admin.web/Models/ModeloEntidadGAHE.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Usuario\Desktop\tesisbufete\ejemplos de codigo\ProyectoHuamani\sb-admin.web\Models\ModeloEntidadGAHE.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\JoseR\Desktop\CAAP EXAMNEN\Despacho Judicial Peru\sistema\sb-admin.web\Models\ModeloEntidadGAHE.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
